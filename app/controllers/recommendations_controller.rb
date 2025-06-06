b9927e16a943274ccc21802510107ac8b51b8725
Aayushi Tomar <ayushitomar2711@gmail.com>
2025-06-06 08:28:14 -0400

    Fixed Address RuboCop linting errors and passing tests

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,6 +1,7 @@
+# app/controllers/recommendations_controller.rb
 class RecommendationsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_recommendation, only: [:show]
+  before_action :authenticate_user!, except: [ :index, :show ] # Added spaces
+  before_action :set_recommendation, only: [ :show ] # Added spaces
 
   def index
     @recommendations = Recommendation.includes(:user, :book).order(created_at: :desc)
@@ -19,7 +20,7 @@ class RecommendationsController < ApplicationController
     @recommendation = current_user.recommendations.build(recommendation_params)
 
     if @recommendation.save
-      redirect_to @recommendation, notice: 'Recommendation was successfully created.'
+      redirect_to @recommendation, notice: "Recommendation was successfully created." # Changed to double quotes
     else
       render :new
     end
@@ -32,7 +33,6 @@ class RecommendationsController < ApplicationController
   end
 
   def recommendation_params
-    params.require(:recommendation).permit(:review, book_attributes: [:id, :title, :author, :image_url])
+    params.require(:recommendation).permit(:review, book_attributes: [ :id, :title, :author, :image_url ]) # Added spaces
   end
 end
-
